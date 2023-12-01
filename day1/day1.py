72d3618826cc3929eb34d8c2be6ab050fbe0334d
Kaiwinta <titouan.pradoura@epitech.eu>
2023-12-01 10:12:47 +0100

    Day1_working

---
+++ day1/day1.py
@@ -0,0 +1,38 @@
+##
+## EPITECH PROJECT, 2023
+## Advent23-01
+## File description:
+## day1.py
+##
+
+f = open("day1/day1_input.txt", "r")
+
+def line_check(fd, char):
+    nb_1 = 0
+    nb_2 = 0
+    counter = 0
+    while (char != '\n'):
+        try:
+            int(char)
+            if counter == 0:
+                nb_1 = int(char)
+                nb_2 = int(char)
+                counter += 1
+            else:
+                nb_2 = int(char)
+                counter +=1
+        except:
+            pass
+        char = fd.read(1)
+    return nb_1 * 10 + nb_2
+
+
+def line_loop(fd):
+    char = fd.read(1)
+    nb = 0
+    while char:
+        nb += (line_check(fd, char))
+        char = fd.read(1)
+    print(nb)
+        
+line_loop(f)
